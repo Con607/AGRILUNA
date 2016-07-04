a37caf4e8a2c94d8f84801abc6adefd1abdc4985
Irving Amador <conexxionbusiness@gmail.com>
2016-07-03 20:00:52 -0500

    images path mod

--- app/models/operating_cost.rb
+++ app/models/operating_cost.rb
@@ -3,6 +3,8 @@ class OperatingCost < ActiveRecord::Base
 	belongs_to :company
 
 	has_attached_file :image1, styles: { :big => "650x550", :front => "581x326", medium: "300x300>", thumb: "100x100>" }, 
-								default_url: "/images/:style/missing.png"
+								:url => "/public/assets/images/administration_costsoperating_costs/:style/:filename", 
+								:path => ":rails_root/public/assets/images/operating_costs/:style/:filename",
+								:default_url => "/public/assets/images/:style/missing.png"
   	validates_attachment_content_type :image1, content_type: /\Aimage\/.*\Z/
 end
