4da06edfa49278e55fc4d43daa9c1f5af4762e14
Irving Amador <conexxionbusiness@gmail.com>
2016-07-03 19:30:28 -0500

    Changing paths for images

--- app/models/administration_cost.rb
+++ app/models/administration_cost.rb
@@ -3,8 +3,10 @@ class AdministrationCost < ActiveRecord::Base
 	has_many :apportionment_per_greenhouses
 	
 
-	has_attached_file :image1, styles: { :big => "650x550", :front => "581x326", medium: "300x300>", thumb: "100x100>" }, 
-								default_url: "/images/:style/missing.png"
+	has_attached_file :image1, styles: { :big => "650x550", :front => "581x326", medium: "300x300>", thumb: "100x100>" },
+								:url => "/public/assets/images/properties/:style/:filename", 
+								:path => ":rails_root/public/assets/images/properties/:style/:filename",
+								:default_url => "/public/assets/images/:style/missing.png"
   	validates_attachment_content_type :image1, content_type: /\Aimage\/.*\Z/
 
 end
