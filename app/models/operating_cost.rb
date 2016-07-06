550192feed3567e5d9b64136de47fdcac5b78520
Irving Amador <conexxionbusiness@gmail.com>
2016-07-06 13:18:31 -0500

    Fixed operating_costs image url

--- app/models/operating_cost.rb
+++ app/models/operating_cost.rb
@@ -3,7 +3,7 @@ class OperatingCost < ActiveRecord::Base
 	belongs_to :company
 
 	has_attached_file :image1, styles: { :big => "650x550", :front => "581x326", medium: "300x300>", thumb: "100x100>" }, 
-								:url => "/public/assets/images/administration_costsoperating_costs/:style/:filename", 
+								:url => "/public/assets/images/operating_costs/:style/:filename", 
 								:path => ":rails_root/public/assets/images/operating_costs/:style/:filename",
 								:default_url => "/public/assets/images/:style/missing.png"
   	validates_attachment_content_type :image1, content_type: /\Aimage\/.*\Z/
