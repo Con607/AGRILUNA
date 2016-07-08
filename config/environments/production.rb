4fd0c36b644cd0efda306720f326084bf53afb75
Irving Amador <conexxionbusiness@gmail.com>
2016-07-08 14:29:13 -0500

    fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,8 @@ Myapp::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  #config.serve_static_assets = false
+  config.serve_static_files = false
   # Apache or NGINX already handles this.
   # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
