bae9bad52e63e498d458ced2308bd0b6292e8e3d
Irving Amador <conexxionbusiness@gmail.com>
2016-07-23 16:25:44 -0500

    Fixing devise version 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :check_if_allowed_controller, if: :user_signed_in?
   before_action :check_if_allowed_action, if: :user_signed_in?
-  #before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   helper_method :is_allowed?
 
@@ -15,7 +16,7 @@ class ApplicationController < ActionController::Base
   protected
 
   	def configure_permitted_parameters
-  	  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :second_lat_name)}
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :second_lat_name])
   	end
 
     def check_if_allowed_controller
@@ -27,7 +28,7 @@ class ApplicationController < ActionController::Base
     def check_if_allowed_action
        if is_allowed?("#{params[:controller]}/#{params[:action]}") != true
         ### Debugging line ###
-        #puts "checking.....  controller = #{params[:controller]}, action = #{params[:action]}, #{params[:controller]}/#{params[:action]}"
+        puts "checking.....  controller = #{params[:controller]}, action = #{params[:action]}, #{params[:controller]}/#{params[:action]}"
         redirect_to root_path, notice: (t :not_enough_permissions)
        end
     end
--- config/initializers/devise_permitted_parameters.rb
+++ config/initializers/devise_permitted_parameters.rb
@@ -7,9 +7,17 @@ module DevisePermittedParameters
 
   protected
 
+  #def configure_permitted_parameters
+    #devise_parameter_sanitizer.for(:sign_up) << :name
+    #devise_parameter_sanitizer.for(:account_update) << :name
+  #end
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit({ roles: [] }, :email, :password, :password_confirmation)
+      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :second_lat_name])
+      																#:phone, :email, bank_attributes: [:bank_name, :bank_account]])
+    end
   end
 
 end
