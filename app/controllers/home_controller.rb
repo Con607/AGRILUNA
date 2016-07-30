6f5d2fcfea2d4634d0be2a9feec7a1db5c6a1d01
Irving Amador <conexxionbusiness@gmail.com>
2016-07-30 03:00:20 -0500

    Small fixes for the login view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
 
 
   def index
-    @mainTitle = "Welcome in Inspinia Rails Seed Project"
+    @mainTitle = "Bienvenido a AgroWeb"
     @mainDesc = "It is an application skeleton for a typical Ruby on Rails web app. You can use it to quickly bootstrap your webapp projects and dev/prod environment."
   end
 
